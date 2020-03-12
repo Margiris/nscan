1f390b8ec8e9eb3d148e8320fcca59cde08e95eb
margiris <margiris.burakauskas@gmail.com>
2020-03-12 16:52:03 +0200

    much refactoring, many work

--- nscan.lua
+++ nscan.lua
@@ -1,79 +1,136 @@
-local nmap = require "nmap"
-local stdnse = require "stdnse"
-local target = require "target"
+-- local nmap = require "nmap"
+local stdnse = require"stdnse"
+local target = require"target"
 
 description = "Takes arguments from console"
-
 categories = {"discovery"}
-
 license = "Same as Nmap--See https://nmap.org/book/man-legal.html"
 
-function get_arguments ()
-    args = {}
+function contains(tbl, value)
+    for _, v in pairs(tbl) do
+        if v == value then
+            return true
+        end
+    end
+    return false
+end
+
+function get_table_keys_list(tbl)
+    local keyset = {}
+    for k, _ in pairs(tbl) do
+        table.insert(keyset, k)
+    end
+    return keyset
+end
+
+function recurse_table_to_json_string(tbl, filter, indentation, prefix_key)
+    if not tbl then
+        return "\n*** Table is nil ***\n"
+    end
+
+    filter = filter or {}
+    indentation = indentation or ""
+    prefix_key = not (not prefix_key or false)
+    local result = ""
+    for k, v in pairs(tbl) do
+        if (type(v) == "table") then
+            if next(v) ~= nil then -- only recurse if table is not empty
+                local deeper = recurse_table_to_json_string(v, filter, "\t" .. indentation, prefix_key)
+                if deeper ~= "" or next(filter) == nil then -- suppress empty table titles if filtering is used
+                    result = result .. indentation .. k .. ":\n" .. deeper
+                end
+            end
+        else
+            if next(filter) == nil or contains(filter, k) then
+                if prefix_key then
+                    result = result .. indentation .. k .. " = "
+                end
+                result = result .. tostring(v) .. "\n"
+            end
+        end
+    end
+    return result
+end
+
+function get_arguments()
+    local args = {}
     args.output_filename = stdnse.get_script_args({"nscan.filename"})
     args.interface = stdnse.get_script_args({"nscan.interface"})
     args.scan_type = stdnse.get_script_args({"nscan.type"})
     return args
 end
 
-function print_table_recursively (tbl, prefix)
-    prefix = prefix or ""
-    for k, v in pairs(tbl) do
-        if (type(v) == "table") then
-            print(prefix .. k .. ":")
-            print_table_recursively(v, prefix .. "\t")
-        else
-            print(prefix .. k .. " = " .. tostring(v))
+function get_network_devices_info(ubus_connection, device_name)
+    local temp = {}
+    if device_name ~= "" and device_name ~= "--list" and device_name ~= "--all" then
+        temp.name = device_name
+    end
+
+    local device_list = ubus_connection:call("network.device", "status", temp)
+    if temp.name == nil then
+        return get_table_keys_list(device_list)
+    end
+    return device_list
+end
+
+function get_network_interface_data(ubus_connection, device_name)
+    return ubus_connection:call("network.interface." .. device_name, "dump", { })
+end
+
+function get_vlans(ubus_connection, device_name)
+    local dump = ubus_connection:call("uci", "get", { config="network", section="@switch_vlan[0]"})
+
+    return dump
+end
+
+function add_targets()
+    success = true
+    targets = {}
+    targets[1] = "192.168.1.214"
+    -- temporarily enable adding new targets irrespective of script arguments and save old value for restoring later
+    local old_ALLOW_NEW_TARGETS = target.ALLOW_NEW_TARGETS
+    target.ALLOW_NEW_TARGETS = true
+
+    for _, item in ipairs(targets) do
+        local st, err = target.add(item)
+
+        if not st then
+            print("\n\nCouldn't add target " .. item .. ": " .. err .. "\n\n")
+            success = false
         end
     end
+
+    -- restore ALLOW_NEW_TARGETS state
+    target.ALLOW_NEW_TARGETS = old_ALLOW_NEW_TARGETS
+
+    return success
 end
 
 prerule = function()
     package.cpath = package.cpath .. ";/usr/lib/lua/?.so"
     local ubus = require "ubus_5_3"
- 
+
     local conn = ubus.connect()
     if not conn then
         error("Failed to connect to ubusd")
     end
 
-    args = get_arguments()
+    local args = get_arguments()
 
-    if args.interface == "--list" then
-        local interface_data = conn:call("network.device", "status", {})
-        for k, _ in pairs(interface_data) do
-            print(k)
-        end
-    else
-        -- Call a procedure
-        local interface_data = conn:call("network.device", "status", { name = args.interface })
-        print(args.interface .. ":")
-        print_table_recursively(interface_data, "\t")
-        print("\nMAC address of interface " .. args.interface .. " is " .. tostring(interface_data["macaddr"]).. "\n")
-    end
+    -- local device_info = get_network_devices_info(conn, args.interface)
+    -- print(recurse_table_to_json_string(device_info, {}, "", true))
 
-    
-    -- Close connection
+    -- local if_data = get_network_interface_data(conn, args.interface)
+    -- print(recurse_table_to_json_string(if_data.interface, {"l3_device", "interface", "mask", "address"}))
+
+    local dump = get_vlans(conn)
+    print(recurse_table_to_json_string(dump))
+
     conn:close()
-    
-    targets = {}
-    targets[1] = "192.168.1.214"
-    
-    -- temporarily enable adding new targets irrespective of script arguments and save old value for restoring later
-    local old_ALLOW_NEW_TARGETS = target.ALLOW_NEW_TARGETS
-    target.ALLOW_NEW_TARGETS = true
-    for _, item in ipairs(targets) do
-        local st, err = target.add(item)
-        if not st then
-            print("\n\nCouldn't add target " .. item .. ": " .. err .."\n\n")
-        end
-    end
-    -- restore ALLOW_NEW_TARGETS state
-    target.ALLOW_NEW_TARGETS = old_ALLOW_NEW_TARGETS
 
-	return true
+    -- add_targets()
+
+    return true
 end
 
-action = function(host, port)
-    return 0
-end
+action = function(host, port) return 0 end
